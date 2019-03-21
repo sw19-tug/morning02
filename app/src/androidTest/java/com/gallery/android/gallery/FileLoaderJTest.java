d8e5c0d481436a1919e34231e95de01e3b06c0ad
luso97 <mcluis1997@gmail.com>
2019-03-21 15:34:31 +0100

    GAL-001 created test for images displaying [LM,GJ]

--- app/src/androidTest/java/com/gallery/android/gallery/FileLoaderJTest.java
+++ app/src/androidTest/java/com/gallery/android/gallery/FileLoaderJTest.java
@@ -2,6 +2,8 @@ package com.gallery.android.gallery;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,6 +11,10 @@ import org.junit.runner.RunWith;
 
 import java.util.List;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertFalse;
 
@@ -23,6 +29,11 @@ public class FileLoaderJTest {
         List<String> paths=f.getImagesPaths();
         assertNotNull(paths);
         assertFalse(paths.isEmpty());
+        ImageContainer iC = new ImageContainer(paths.get(0));
+        assertFalse(iC.getPath() == paths.get(0));
+        assertNotNull(iC.getImage());
 
+        ImageView iView1=activityTestRule.getActivity().findViewById(R.id.image01);
+        assertNotNull(iView1.getDrawable());
     }
 }

acbda8af4a2d0010d6475124b7824ba209bbcf60
luso97 <mcluis1997@gmail.com>
2019-03-21 15:44:00 +0100

    GAL-001 first images displaying [LM,GJ]

--- app/src/main/java/com/gallery/android/gallery/MainActivity.java
+++ app/src/main/java/com/gallery/android/gallery/MainActivity.java
@@ -2,7 +2,10 @@ package com.gallery.android.gallery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FileLoader loader = new FileLoader();
+        List<String> paths = loader.getImagesPaths();
+        List<ImageContainer> imageList = new ArrayList<ImageContainer>();
+        for(int i = 0; i < paths.size(); i++ )
+        {
+            imageList.add(new ImageContainer(paths.get(i)));
+        }
 
+        ImageView firstIV = (ImageView) findViewById(R.id.image01);
+        firstIV.setImageBitmap(imageList.get(0).getImage());
     }
 }

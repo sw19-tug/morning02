74982794fd006afbec1097deae614c5ce572f24d
luso97 <mcluis1997@gmail.com>
2019-03-21 14:38:03 +0100

    FileLoader class functions implemented and Android Manifest  changed [LM,GJ]

--- app/src/main/java/com/gallery/android/gallery/FileLoader.java
+++ app/src/main/java/com/gallery/android/gallery/FileLoader.java
@@ -14,7 +14,29 @@ public class FileLoader implements FileLoaderInterface {
         this.bitmapList=new ArrayList<Bitmap>();
     }
     private List<Bitmap> bitmapList;
+    @Override
+    public Bitmap getAndAddBitMap(String s) {
+        Bitmap b= BitmapFactory.decodeFile(s);
+        bitmapList.add(b);
+        return b;
+    }
 
+    @Override
+    public ImageView getImages(ImageView view,int index) {
+        view.setImageBitmap(bitmapList.get(index));
+        return view;
+    }
+    public List<String> getImagesPaths() {
+        //String path = Environment.getExternalStorageDirectory().toString()+"/Pictures";
+        String path = "/storage/sdcard/DCIM/Camera";
+        File dir = new File(path);
+        File[] filelist = dir.listFiles();
+        List<String> paths = new ArrayList<String>();
+        for (File f : filelist) {
+            paths.add(f.getAbsolutePath());
+        }
+        return paths;
+    }
 
 
 
--- app/src/main/java/com/gallery/android/gallery/FileLoaderInterface.java
+++ app/src/main/java/com/gallery/android/gallery/FileLoaderInterface.java
@@ -7,5 +7,7 @@ import java.util.List;
 
 public interface FileLoaderInterface {
 
-
+    Bitmap getAndAddBitMap(String s);
+    ImageView getImages(ImageView view,int index);
+    List<String> getImagesPaths();
 }

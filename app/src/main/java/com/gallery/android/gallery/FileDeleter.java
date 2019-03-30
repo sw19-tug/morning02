75e8dfade74d06fe42dffccadbd875100f9e6186
PalomeroLopez <palomerolopez@student.tugraz.at>
2019-03-30 17:38:34 +0100

    GAL-006 Implement delete function [GJ,DP]

--- app/src/main/java/com/gallery/android/gallery/FileDeleter.java
+++ app/src/main/java/com/gallery/android/gallery/FileDeleter.java
@@ -15,6 +15,21 @@ public class FileDeleter implements FileDeleterInterface{
     public boolean delete(String path) {
 
         boolean deleted = false;
+        File file = new File(path);
+        if(file.exists())
+        {
+            deleted = file.delete();
+            if(deleted)
+                System.out.println("File deleted: " + path);
+            else
+                System.out.println("File could not be deleted: " + path);
+        }
+        else
+        {
+            System.out.println("File not found: " + path);
+            deleted = false;
+        }
+
         return deleted;
     }
 

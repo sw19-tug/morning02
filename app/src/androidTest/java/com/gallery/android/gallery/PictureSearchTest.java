f34cd83edfd9da38440a5143822341ce11e34e07
estettner <emma.stettner@student.tugraz.at>
2019-04-05 11:59:56 +0200

    GAL-005 TextField for Searchbar [RA, ES]

--- app/src/androidTest/java/com/gallery/android/gallery/PictureSearchTest.java
+++ app/src/androidTest/java/com/gallery/android/gallery/PictureSearchTest.java
@@ -10,8 +10,11 @@ import org.junit.runner.RunWith;
 import android.support.test.rule.ActivityTestRule;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -24,8 +27,13 @@ public class PictureSearchTest {
 
     @Test
     public void testSearchbarVisible() {
-        onView(withId(R.id.searchbar)).check(matches(isDisplayed()));
+        onView(withId(R.id.search_bar)).check(matches(isDisplayed()));
 
+    }
+
+    @Test
+    public void testSearchButton() {
+        onView(withId(R.id.search_button)).check(matches(isDisplayed()));
     }
 
 

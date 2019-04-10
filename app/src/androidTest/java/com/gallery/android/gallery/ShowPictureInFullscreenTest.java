84a9931a57a3cbe6a5facac3fd3b24531a28e070
Gernot Jank <gernot.jank@student.tugraz.at>
2019-04-10 12:55:05 +0200

    release-v0.0.2 Fix ShowPictureInFullscrenTest [GJ,CW]

--- app/src/androidTest/java/com/gallery/android/gallery/ShowPictureInFullscreenTest.java
+++ app/src/androidTest/java/com/gallery/android/gallery/ShowPictureInFullscreenTest.java
@@ -1,14 +1,23 @@
 package com.gallery.android.gallery;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Environment;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.widget.RecyclerView;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -17,10 +26,48 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
 import static org.junit.Assert.assertEquals;
 
-
 @RunWith(AndroidJUnit4.class)
 public class ShowPictureInFullscreenTest {
 
+    @BeforeClass
+    public static void setUpClass() {
+        //create test.png
+        String name = "test.png";
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
+        File dir = new File(path);
+        if(!dir.exists() && !dir.isDirectory()){
+            if(!dir.mkdirs())
+                System.out.println("ERROR: Not able to create a test image!");
+        }
+        File dest = new File(path, name);
+        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        for(int x = 0; x < 100; x++){
+            for(int y = 0; y < 100; y++){
+                bitmap.setPixel(x, y, Color.rgb(2, 100, 56));
+            }
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(dest);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws IOException {
+        //delete test.png
+        String name = "test.png";
+        String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
+        File file = new File(path, name);
+        if(file.exists())
+        {
+            file.delete();
+        }
+    }
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
@@ -35,48 +82,20 @@ public class ShowPictureInFullscreenTest {
 
     @Test
     public void testClick() throws Throwable, InterruptedException {
-
-       //String path_check = "";
-
-        RecyclerView res = activityTestRule.getActivity().findViewById(R.id.RecyclerId);
-        AdapterImages ad2 = (AdapterImages) res.getAdapter();
-
-        for (int i = 0; i < ad2.getItemCount(); i++ ) {
-
-
-            runOnUiThread(new MyRunnable(res, i) {
-
+        RecyclerView recycler_view = activityTestRule.getActivity().findViewById(R.id.RecyclerId);
+        AdapterImages adapter_images = (AdapterImages) recycler_view.getAdapter();
+        for (int i = 0; i < adapter_images.getItemCount(); i++ ) {
+            runOnUiThread(new MyRunnable(recycler_view, i) {
                 public void run() {
-
-                    AdapterImages ad1 = (AdapterImages) this.resycler_view.getAdapter();
-
-                    //path_check = ad.getListImages().get(i).getPath();
                     this.resycler_view.findViewHolderForAdapterPosition(adapter_position).itemView.performClick();
-
-
                     try {
                         Thread.sleep(100);
-
-
-
                     } catch (InterruptedException ex1) {
                         return;
                     }
-
                 }
             });
-
-
-
             onView(withId(R.id.fullscreen_image_view)).check(matches(isDisplayed()));
-
-
-
-
         }
-
-
-
     }
-
 }

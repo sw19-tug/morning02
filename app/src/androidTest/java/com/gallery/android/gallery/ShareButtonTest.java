f2d9e5ca155af0a4bf007e25fbd701c89080b3c2
luso97 <mcluis1997@gmail.com>
2019-04-11 19:38:19 +0200

    Fixed ShareButtonTest for newer versions [LM,PS]

--- app/src/androidTest/java/com/gallery/android/gallery/ShareButtonTest.java
+++ app/src/androidTest/java/com/gallery/android/gallery/ShareButtonTest.java
@@ -1,28 +1,35 @@
 package com.gallery.android.gallery;
 
+//import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+//import static androidx.test.ext.truth.content.IntentSubject.assertThat;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Environment;
 import android.support.test.espresso.NoMatchingViewException;
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.File;
 import java.io.FileOutputStream;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
+@RunWith(AndroidJUnit4.class)
 public class ShareButtonTest {
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> activityTestRule =
+            new IntentsTestRule<>(MainActivity.class);
+
     @Test
     public void testDialogIsShown(){
         try {
@@ -38,7 +45,8 @@ public class ShareButtonTest {
             }
             File dest = new File(path, name);
 
-            Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+            Bitmap bitmap;
+            bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
             for(int x = 0; x < 100; x++){
                 for(int y = 0; y < 100; y++){
                     bitmap.setPixel(x, y, Color.rgb(2, 100, 56));
@@ -58,9 +66,13 @@ public class ShareButtonTest {
             onView(withId(R.id.idImage)).perform(click());
         }
         onView(withId(R.id.shareButton)).perform(click());
-        onView(withText("Share Using")).check(matches(isDisplayed()));
 
-        //delete test.png
+
+        intending(hasAction(Intent.ACTION_SEND));
+
+
+
+
         String name = "test.png";
         String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
         File file = new File(path, name);

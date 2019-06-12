bcb37653ef0ab22b619aaf92fc68546ba5b63649
PalomeroLopez <48415120+PalomeroLopez@users.noreply.github.com>
2019-06-12 08:41:25 +0200

    GAL-007 Fixed AddImage Test [LM,DP]

--- app/src/androidTest/java/com/gallery/android/gallery/AddImageTest.java
+++ app/src/androidTest/java/com/gallery/android/gallery/AddImageTest.java
@@ -1,11 +1,14 @@
 package com.gallery.android.gallery;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Environment;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.util.Log;
 
 import org.junit.AfterClass;
 import org.junit.Rule;
@@ -14,6 +17,7 @@ import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
 import java.io.File;
+import java.util.Collection;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
@@ -28,12 +32,18 @@ import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.runner.lifecycle.Stage.CREATED;
+import static android.support.test.runner.lifecycle.Stage.PAUSED;
+import static android.support.test.runner.lifecycle.Stage.PRE_ON_CREATE;
+import static android.support.test.runner.lifecycle.Stage.RESUMED;
+import static android.support.test.runner.lifecycle.Stage.STARTED;
+import static android.support.test.runner.lifecycle.Stage.STOPPED;
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class AddImageTest {
     private ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
-    private ActivityTestRule<AddImageActivity> addImageRule;
+    private ActivityTestRule<AddImageActivity> addImageRule=new ActivityTestRule<AddImageActivity>(AddImageActivity.class);
 
 
     @Rule
@@ -41,13 +51,9 @@ public class AddImageTest {
             .outerRule(GrantPermissionRule.grant(
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE))
-            //.around(activityTestRule);
-            .around(addImageRule = new ActivityTestRule<AddImageActivity>(AddImageActivity.class) {
-        protected void beforeActivityLaunched() {
-
-        }
-    })
-            .around(activityTestRule);
+            .around(activityTestRule)
+            //.around(addImageRule = new ActivityTestRule<AddImageActivity>(AddImageActivity.class))
+                    .around(activityTestRule);
 
 
 
@@ -58,8 +64,11 @@ public class AddImageTest {
     public void cameraStarted() {
         Intent intent2=new Intent();
         activityTestRule.launchActivity(intent2);
+        Intent intent=new Intent();
+        addImageRule.launchActivity(intent);
         Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
         String r=addImageRule.getActivity().savebitmap(bitmap);
+        addImageRule.finishActivity();
         String extStorageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
         File file = new File(extStorageDirectory, "QR_" + r + ".png");
         assertTrue(file.exists());
@@ -73,9 +82,30 @@ public class AddImageTest {
         /*Intent intent=new Intent();
         activityTestRule.launchActivity(intent);*/
         assertNotNull(activityTestRule.getActivity().findViewById(R.id.addImage));
-        activityTestRule.finishActivity();
 
 
 
+
+    }
+
+
+    Activity currentActivity = null;
+
+    public Activity getActivityInstance(){
+        getInstrumentation().runOnMainSync(new Runnable() {
+            public void run() {
+                Collection<Activity> resumedActivities =
+                        ActivityLifecycleMonitorRegistry.getInstance().getActivitiesInStage(RESUMED);
+                Log.e("holahola",String.valueOf(resumedActivities.size()));
+                boolean res=false;
+                for(Activity a:resumedActivities){
+                    System.out.println(a.getTitle());
+                    currentActivity=a;
+                    Log.e("holahola",String.valueOf(a.getTitle()));
+                }
+            }
+        });
+
+        return currentActivity;
     }
 }

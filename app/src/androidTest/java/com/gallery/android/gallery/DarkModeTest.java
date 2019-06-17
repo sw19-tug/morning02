e73db6a22f4ab55f88d89f1db3d7281ce7802ea1
luso97 <mcluis1997@gmail.com>
2019-06-17 18:33:44 +0200

    GAL-021 Feature started with not working test [LM,PS]

---
+++ app/src/androidTest/java/com/gallery/android/gallery/DarkModeTest.java
@@ -0,0 +1,51 @@
+package com.gallery.android.gallery;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
+import android.support.v7.widget.SwitchCompat;
+import android.text.Layout;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertNotEquals;
+
+public class DarkModeTest {
+    public ActivityTestRule<MainActivity> activityTestRule;
+    @Rule
+    public final TestRule chain = RuleChain
+            .outerRule(GrantPermissionRule.grant(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            .around(activityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class) {
+
+            });
+
+    @Test
+    public void buttonExists(){
+        onView(withId(R.id.NModeswitchAB)).perform(click());
+    }
+
+
+}
